4b0a6cb987b197a4b92171037c1686fda235e741
bitbytejoy <bitbytejoy@gmail.com>
2022-10-23 00:24:07 +0200

    BIG BANG

---
+++ src/test/java/com/bitbytejoy/neuefischetodo/NeuefischeTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitbytejoy.neuefischetodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuefischeTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
