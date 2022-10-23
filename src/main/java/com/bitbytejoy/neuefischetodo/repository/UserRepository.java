ded74a923be594ccbac67f44774ab8998a017be4
bitbytejoy <bitbytejoy@gmail.com>
2022-10-23 19:52:32 +0200

    fix: Code styling

--- src/main/java/com/bitbytejoy/neuefischetodo/repository/UserRepository.java
+++ src/main/java/com/bitbytejoy/neuefischetodo/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.bitbytejoy.neuefischetodo.repository;
 import com.bitbytejoy.neuefischetodo.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository  extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String> {
     User findByEmail(String email);
     boolean existsByEmail(String email);
 }
